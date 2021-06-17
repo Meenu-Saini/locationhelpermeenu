5fb7a108a7f9fb03ee0f3c33176942bc4089da59
Meenu Saini <meenusaini9999@gmail.com>
2021-06-18 00:06:17 +0530

    initial commit

---
+++ mylibrary/src/main/java/com/uneem/mylibrary/LocationHelper.java
@@ -0,0 +1,12 @@
+package com.uneem.mylibrary;
+
+import android.location.Location;
+
+public class LocationHelper {
+    public static float getDistanceInKm(double startLatitude,double startLongitude,double endLatitude,
+                                        double endLongitude){
+        float[] result=new float[0];
+        Location.distanceBetween(startLatitude,startLongitude,endLatitude,endLongitude,result);
+        return result[0]/1000;
+    }
+}
